44153868504f7450f6b68f29abbdff43352e6345
baba-s <shotababa1206@gmail.com>
2022-12-24 15:59:19 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Kogane.PolygonCollider2DExtensionMethods
+# Kogane Polygon Collider 2D Extension Methods
+
+PolygonCollider2D の拡張メソッド
+
+## 使用例
+
+```cs
+// 指定されたスプライトの形に合わせて PolygonCollider2D を更新します
+collider.UpdateFromSprite( sprite );
+```
---
+++ Runtime/PolygonCollider2DExtensionMethods.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kogane
+{
+    /// <summary>
+    /// PolygonCollider2D の拡張メソッド
+    /// </summary>
+    public static class PolygonCollider2DExtensionMethods
+    {
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// 指定されたスプライトの形に合わせて PolygonCollider2D を更新します
+        /// </summary>
+        public static void UpdateFromSprite( this PolygonCollider2D self, SpriteRenderer spriteRenderer )
+        {
+            self.UpdateFromSprite( spriteRenderer.sprite );
+        }
+
+        /// <summary>
+        /// 指定されたスプライトの形に合わせて PolygonCollider2D を更新します
+        /// </summary>
+        public static void UpdateFromSprite( this PolygonCollider2D self, Sprite sprite )
+        {
+            var physicsShapeCount = sprite.GetPhysicsShapeCount();
+
+            self.pathCount = physicsShapeCount;
+
+            var physicsShape = new List<Vector2>();
+
+            for ( var i = 0; i < physicsShapeCount; i++ )
+            {
+                physicsShape.Clear();
+                sprite.GetPhysicsShape( i, physicsShape );
+                var points = physicsShape.ToArray();
+                self.SetPath( i, points );
+            }
+        }
+    }
+}
